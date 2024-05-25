24bc8bb1e8e911c5e24df71f071c7c2bc2459ba7
Vaishnavi Saxena <111011311+niteangel2@users.noreply.github.com>
2024-05-25 23:04:26 +0530

    Time: 165 ms (5.06%), Space: 44.4 MB (46.96%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode head1, ListNode head2){
+        ListNode merged = new ListNode(-1);
+        ListNode temp = merged;
+        while(head1 != null && head2 != null){
+            if(head1.val <= head2.val){
+                temp.next = head1;
+                head1 = head1.next;
+            }
+            else{
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return merged.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+        return null;
+        if(lists.length == 1){
+            return lists[0];
+        }
+        for(int i = 1; i < lists.length;i++){
+            lists[i] = merge(lists[i],lists[i-1]);
+        }
+        return lists[lists.length - 1];
+    }
+}
