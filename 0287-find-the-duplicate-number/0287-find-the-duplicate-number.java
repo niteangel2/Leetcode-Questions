a93025e3425511b74cf90755ee2d377d57d1f6c2
Vaishnavi Saxena <111011311+niteangel2@users.noreply.github.com>
2024-10-14 01:37:22 +0530

    Time: 22 ms (27.97%), Space: 58.1 MB (64.48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,10 +1,19 @@
-class Solution { //visiting modifying input array
-    public int findDuplicate(int[] nums) {
-        for(int i : nums) {
-            int idx = Math.abs(i);
-            if(nums[idx] < 0) return idx;
-            nums[idx] = -nums[idx];
+class Solution {
+    public int count(int[] nums, int x) {
+        int cnt = 0;
+        for(int i = 0; i < nums.length;i++) {
+            if(nums[i] <= x) cnt++;
         }
-        return 0;
+        return cnt;
+    }
+    public int findDuplicate(int[] nums) { // using binary search on answer TC : O(nlogn) SC : O(1)
+        int s = 1;
+        int e = nums.length;
+        while(s < e) {
+            int mid = s + (e-s)/2;
+            if(count(nums,mid) > mid) e = mid;
+            else s = mid+1;
+        }
+        return s;
     }
 }
