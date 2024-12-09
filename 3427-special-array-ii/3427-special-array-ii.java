c36045e0d4850c8477187b368da0f095edf5585a
Vaishnavi Saxena <111011311+niteangel2@users.noreply.github.com>
2024-12-10 02:24:39 +0530

    Time: 6 ms (24.29%), Space: 111.4 MB (6.03%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        boolean[] ans = new boolean[queries.length];
+        ArrayList<Integer> violatingIndices = new ArrayList<>();
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                violatingIndices.add(i);
+            }
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            int start = query[0];
+            int end = query[1];
+
+            boolean foundViolatingIndex = binarySearch(
+                start + 1,
+                end,
+                violatingIndices
+            );
+
+            if (foundViolatingIndex) {
+                ans[i] = false;
+            } else {
+                ans[i] = true;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean binarySearch(
+        int start,
+        int end,
+        ArrayList<Integer> violatingIndices
+    ) {
+        int left = 0;
+        int right = violatingIndices.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int violatingIndex = violatingIndices.get(mid);
+
+            if (violatingIndex < start) {
+                left = mid + 1;
+            } else if (violatingIndex > end) {
+                right = mid - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
