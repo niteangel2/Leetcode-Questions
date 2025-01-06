b5e8212e498414fd49cb69082cd91991d6780023
Vaishnavi Saxena <111011311+niteangel2@users.noreply.github.com>
2025-01-07 01:54:10 +0530

    Time: 9 ms (27.05%), Space: 42.9 MB (20.6%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger s1=new BigInteger(num1);
+BigInteger s2=new BigInteger(num2);
+        BigInteger s3=s1.multiply(s2);
+        
+        return s3.toString();
+
+    }
+}
